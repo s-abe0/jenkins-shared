2ba4861539be2a18b2c58ce94b79d41b113f0f15
Shane Abe <sabe@intellective.com>
2020-12-17 13:38:42 -0500

    update script

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,8 +1,4 @@
 def call(Map stageParams) {
 
-	checkout([
-		$class: 'GitSCM',
-		branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
+	git url: stageParams.url
 }
